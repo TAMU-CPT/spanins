e39739c35cca358e326f51fc9aa51afd8f6d86c3
Eleni Mijalis <elenimijalis@gmail.com>
2016-09-09 13:36:27 -0500

    fix and migration

--- base/models.py
+++ base/models.py
@@ -17,13 +17,13 @@ class Host(models.Model):
 class Spanin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sequence = models.TextField()
-    accession = models.CharField(max_lenth=64)
+    accession = models.CharField(max_length=64)
 
 class Phage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     host = models.ForeignKey(Host)
     name = models.CharField(max_length=128)
-    accession = models.CharField(max_lenth=64)
+    accession = models.CharField(max_length=64)
     spanin_type = models.IntegerField(choices=SPANIN_TYPES)
     i_spanin = models.ForeignKey(Spanin, related_name="i_spanin", null=True)
     o_spanin = models.ForeignKey(Spanin, related_name="o_spanin", null=True)
